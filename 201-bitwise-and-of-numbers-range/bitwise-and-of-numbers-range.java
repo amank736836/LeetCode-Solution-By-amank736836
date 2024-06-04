8d7f5a34e76f4636a9981d8f11dbecbc9882e730
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-06-04 09:24:24 +0530

    Time: 3 ms (100.00%) | Memory: 43.4 MB (90.42%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -1,6 +1,9 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        var shift = 31 ^ Integer.numberOfLeadingZeros(left ^ right | 1);
-        return left >>> shift << shift;
+        while (right > left) {
+            right = right & (right - 1);
+        }
+
+        return right;
     }
 }
