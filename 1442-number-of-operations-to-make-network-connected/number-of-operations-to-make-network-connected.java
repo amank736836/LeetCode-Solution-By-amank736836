5fced2b0ad3d6e0ffc6c5a3e42c442ecc03eceee
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-07-19 16:14:17 +0530

    Time: 7 ms (66.45%) | Memory: 60.9 MB (72.97%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,39 +1,88 @@
-class Solution {
-    boolean visited[];
-    List<Integer> adj[];
-    public int makeConnected(int n, int[][] connections) {
-        if(connections.length < n-1){
-            return -1;
-        }
+class DisjointSet{
+    int parent[];
+    int size[];
+    int rank[];
 
-        visited = new boolean[n];
-        adj = new ArrayList[n];
-
+    DisjointSet(int n){
+        parent = new int[n+1];
+        size = new int[n+1];
+        rank = new int[n+1];
         for(int i=0;i<n;i++){
-            adj[i] = new ArrayList<>();
+            parent[i] = i;
+            size[i] = 1;
+            rank[i] = 0;
         }
-
-        for(int edge[] : connections){
-            adj[edge[0]].add(edge[1]);
-            adj[edge[1]].add(edge[0]);
-        }
-        int components = 0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i);
-                components++;
-            }
-        }
-        return components - 1;
     }
 
-    private void dfs(int node){
-        visited[node] = true;
+    int findUltimateParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUltimateParent(parent[node]);
+    }
 
-        for(int neighbour : adj[node]){
-            if(!visited[neighbour]){
-                dfs(neighbour);
+    void unionByRank(int u,int v){
+        int ultimateParentU = findUltimateParent(u);
+        int ultimateParentV = findUltimateParent(v);
+        if(ultimateParentU == ultimateParentV){
+            return;
+        }
+        if(rank[ultimateParentU] < rank[ultimateParentV]){
+            parent[ultimateParentU] = ultimateParentV;
+        }
+        else if(rank[ultimateParentV] < rank[ultimateParentU]){
+            parent[ultimateParentV] = ultimateParentU;
+        }
+        else{
+            parent[ultimateParentV] = ultimateParentU;
+            rank[ultimateParentU]++;
+        }
+    }
+
+    void unionBySize(int u,int v){
+        int ultimateParentU = findUltimateParent(u);
+        int ultimateParentV = findUltimateParent(v);
+        if(ultimateParentU == ultimateParentV){
+            return;
+        }
+
+        if(size[ultimateParentU] < size[ultimateParentV]){
+            parent[ultimateParentU] = ultimateParentV;
+            size[ultimateParentV] += size[ultimateParentU];
+        }
+        else{
+            parent[ultimateParentV] = ultimateParentU;
+            size[ultimateParentU] += size[ultimateParentV];
+        }
+    }
+
+}
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet djs = new DisjointSet(n);
+
+        int extraEdges = 0;
+        for(int edge[] : connections){
+            int u = edge[0];
+            int v = edge[1];
+            if(djs.findUltimateParent(u) == djs.findUltimateParent(v)){
+                extraEdges++;
+            }else{
+                djs.unionBySize(u,v);
             }
         }
+
+        int componentsCount = 0;
+        for(int i=0;i<n;i++){
+            if(djs.findUltimateParent(i) == i){
+                componentsCount++;
+            }
+        }
+
+        if(extraEdges >= componentsCount - 1){
+            return componentsCount-1;
+        }
+        return -1;
     }
 }
