f555dcff0fb86a2ff173d2a0c9be0fa99eaf4dba
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-08-18 10:57:31 +0530

    Time: 51 ms (18.62%) | Memory: 44.8 MB (8.79%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int primes[] = {2,3,5};
+        PriorityQueue<Long> uglyHeap = new PriorityQueue<>();
+        HashSet<Long> visited = new HashSet<>();
+        uglyHeap.add(1L);
+        visited.add(1L);
+        long curr = 1L;
+        for(int i=0;i<n;i++){
+            curr = uglyHeap.poll();
+            for(int prime : primes){
+                long newUgly = curr * prime;
+                if(!visited.contains(newUgly)){
+                    uglyHeap.add(newUgly);
+                    visited.add(newUgly);
+                }
+            }
+        }
+        return (int)curr;
+    }
+}
