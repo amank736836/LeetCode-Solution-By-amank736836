2ed523c1199f9bcbc49b4c69492758b9b55cb01c
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-08-29 13:50:46 +0530

    Time: 3 ms (97.82%) | Memory: 45.1 MB (27.78%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,94 +1,31 @@
-class DisjointSet{
-    int parent[];
-    int rank[];
-    int size[];
-    public DisjointSet(int n){
-        parent = new int[n];
-        rank = new int[n];
-        size = new int[n];
-        for(int i=0;i<n;i++){
-            parent[i] = i;
-            rank[i] = 0;
-            size[i] = 1;
+class Solution {
+    public int numOfIslands = 0;
+    public void unionSets(int a,int b,int parent[]){
+        int parA = findParent(a,parent);
+        int parB = findParent(b,parent);
+        if(parA != parB){
+            parent[parB] = parA;
+            numOfIslands--;
         }
     }
 
-    public int findUltimateParent(int node){
-        if(node == parent[node]){
+    public int findParent(int node,int parent[]){
+        if(parent[node] == 0){
+            parent[node] = node;
+            numOfIslands++;
+        }
+        if(parent[node] == node){
             return node;
         }
-        return parent[node] = findUltimateParent(parent[node]);
+        int par = findParent(parent[node],parent);
+        return par;
     }
 
-    public void unionByRank(int u,int v){
-        int ultimateParentU = findUltimateParent(u);
-        int ultimateParentV = findUltimateParent(v);
-
-        if(ultimateParentU == ultimateParentV){
-            return;
-        }
-        if(rank[ultimateParentU] < rank[ultimateParentV]){
-            parent[ultimateParentU] = ultimateParentV;
-        }
-        else if(rank[ultimateParentV] < rank[ultimateParentU]){
-            parent[ultimateParentV] = ultimateParentU; 
-        }
-        else{
-            parent[ultimateParentU] = ultimateParentV;
-            rank[ultimateParentV]++;
-        }
-    }
-
-    public void unionBySize(int u,int v){
-        int ultimateParentU = findUltimateParent(u);
-        int ultimateParentV = findUltimateParent(v);
-
-        if(ultimateParentU == ultimateParentV){
-            return;
-        }
-        if(size[ultimateParentU] < size[ultimateParentV]){
-            parent[ultimateParentU] = ultimateParentV;
-            size[ultimateParentV] += size[ultimateParentU];
-        }
-        else{
-            parent[ultimateParentV] = ultimateParentU;
-            size[ultimateParentU] += size[ultimateParentV];
-        }
-    }
-}
-
-
-class Solution {
     public int removeStones(int[][] stones) {
-        int maxRow = 0;
-        int maxCol = 0;
+        int parent[] = new int[20003];
         for(int stone[] : stones){
-            maxRow = Math.max(maxRow,stone[0]);
-            maxCol = Math.max(maxCol,stone[1]);
+            unionSets(stone[0] + 1 , stone[1] + 10002,parent);
         }
-
-        DisjointSet djs = new DisjointSet(maxRow + maxCol + 2);
-
-        HashMap<Integer,Integer> stoneNodes = new HashMap<>();
-
-        for(int stone[] : stones){
-            int nodeRow = stone[0];
-            int nodeCol = stone[1] + maxRow + 1;
-            djs.unionBySize(nodeRow,nodeCol);
-            stoneNodes.put(nodeRow , 1);
-            stoneNodes.put(nodeCol , 1);
-        }
-
-        int cnt = 0;
-
-        for(Map.Entry<Integer,Integer> stone : stoneNodes.entrySet()){
-            if(djs.findUltimateParent(stone.getKey()) == stone.getKey()){
-                cnt++;
-            }
-        }
-
-        int n = stones.length;
-
-        return n - cnt;
+        return stones.length - numOfIslands;
     }
 }
